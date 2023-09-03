5eeb0f3972b8e871847b381816bc9239ec12bdf6
Forzenice <Lim.chong.yee@hotmail.com>
2023-09-03 22:26:37 +0800

    Main Function Working
    Main Function Working
    Change API call function list into a Function on it own

--- MalXML.py
+++ MalXML.py
@@ -1,18 +1,29 @@
 import xml.etree.ElementTree as ET
 import requests
+import json
 
 #reterieve season.
-api_url = "https://api.jikan.moe/v4/seasons/2022/Winter"
-response = requests.get(api_url)
 
-response.json()
+api_url = "https://api.jikan.moe/v4/seasons/2023/Winter"
+response = requests.get(api_url)
+seasonJson = response.json()
+totalPage = seasonJson['pagination']['last_visible_page']
+
+seasonList = []
+for x in range(1,totalPage+1):
+    api_url = "https://api.jikan.moe/v4/seasons/2023/Winter?page=" + str(x)
+    response = requests.get(api_url)
+    seasonJson = response.json()
+    for i in range (0,len(seasonJson['data'])):
+        seasonList.append(seasonJson['data'][i]['title'])
+
+print("Season List Len")
+print(len(seasonList))
+print("--------------------")
 #convert this to file
 
-
-tree = ET.parse('animelist Copy.xml')
-
+tree = ET.parse('animelist.xml')
 root = tree.getroot()
-
 totalscore = 0
 count = 0
 
@@ -29,6 +40,8 @@ class animeScore:
     def getScore(self):
         return str(self.score)
 
+
+    
 animelist = []
 
 for child in root.findall('anime'):
@@ -38,12 +51,20 @@ for child in root.findall('anime'):
     enddate = child.find("my_finish_date").text
     status = child.find("my_status").text
     
-    if "2022" in startdate and score != "0" and status == "Completed":
+    if name in seasonList and score != "0" and status == "Completed":
         totalscore += int(score)
         count += 1
         animelist.append(animeScore(name, score))
 
+    '''
+    if "2022" in startdate and score != "0" and status == "Completed":
+        totalscore += int(score)
+        count += 1
+        animelist.append(animeScore(name, score))
+    '''
+
 animelist.sort(key = lambda x: int(x.score),  reverse=True)
+
 
 print()
 for i in animelist:
