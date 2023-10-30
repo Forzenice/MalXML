9fa10012654dabe68ea1225047f548c5402dfe8d
Forzenice <Lim.chong.yee@hotmail.com>
2023-10-30 22:49:22 +0800

    Stats Update

--- MalXML.py
+++ MalXML.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import requests
 import time
+import statistics
 
 #Request Function into JSON
 def getRequest(url):
@@ -13,12 +14,12 @@ seasonList = []
 for s in seasons:
 
     #reterieve total page
-    api_url = "https://api.jikan.moe/v4/seasons/2022/" + s
+    api_url = "https://api.jikan.moe/v4/seasons/2023/" + s
     totalPage = getRequest(api_url)['pagination']['last_visible_page']
 
     #get all in season
     for x in range(1,totalPage+1):
-        api_url = "https://api.jikan.moe/v4/seasons/2022/"+ s + "?page=" + str(x)
+        api_url = "https://api.jikan.moe/v4/seasons/2023/"+ s + "?page=" + str(x)
         seasonJson = getRequest(api_url)
         for i in range (0,len(seasonJson['data'])):
             seasonList.append(seasonJson['data'][i]['title'])
@@ -65,12 +66,28 @@ for child in root.findall('anime'):
 
 animelist.sort(key = lambda x: int(x.score),  reverse=True)
 
-
+sumarr=[]
 print()
 for i in animelist:
     print(i.getScore() + " | " + i.getName())
+
+for x in range (0,10):
+    sumarr.append(sum(1 for u in animelist if u.getScore() == str(x+1)))
 
 print() 
 print("TotalScore : " + str(totalscore))
 print("Count : " + str(count))
 print("Average Score : " + str(totalscore / count))
+print("stdev : " + str(statistics.stdev([int(x.getScore()) for x in animelist])))
+print("Median : " + str(statistics.median([int(x.getScore()) for x in animelist])))
+print("Mode : " + str(statistics.mode([int(x.getScore()) for x in animelist])))
+
+print()
+print("Count")
+print("[1  2  3  4  5  6  7  8  9  10]")
+print(sumarr)
+
+print("1 to 5")
+print(sumarr[0] + sumarr[1] + sumarr[2] + sumarr[3] + sumarr[4])
+print("6 to 10")
+print(sumarr[5] + sumarr[6] + sumarr[7] + sumarr[8] + sumarr[9])
